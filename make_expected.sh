4c67810d7b26c027d61b0679b342615c60164be9
MPL <mpl_pl@poczta.onet.pl>
2019-03-30 11:39:46 +0100

    updated expected.rar to contain test 47

--- make_expected.sh
+++ make_expected.sh
@@ -1,46 +1,53 @@
-./unrar x -y -ad unrar_test_01.rar
-./unrar x -y -ad unrar_test_02.rar
-./unrar x -y -ad unrar_test_03.rar
-./unrar x -y -ad -pqwerty unrar_test_04.rar
-./unrar x -y -ad -pqwerty unrar_test_05.rar
-./unrar x -y -ad unrar_test_06.rar
-./unrar x -y -ad unrar_test_07.rar
-./unrar x -y -ad unrar_test_08.rar
-./unrar x -y -ad unrar_test_09.rar
-./unrar x -y -ad unrar_test_10.rar
-./unrar x -y -ad unrar_test_11.rar
-./unrar x -y -ad unrar_test_12.rar
-./unrar x -y -ad unrar_test_13.rar
-./unrar x -y -ad unrar_test_14.rar
-./unrar x -y -ad unrar_test_15.rar
-./unrar x -y -ad unrar_test_16.rar
-./unrar x -y -ad unrar_test_17.part1.rar
-./unrar x -y -ad unrar_test_18.rar
-./unrar x -y -ad -pżółć unrar_test_19.rar
-./unrar x -y -ad unrar_test_20.rar
-./unrar x -y -ad unrar_test_21.rar
-./unrar x -y -ad -pżółć unrar_test_22.rar
-./unrar x -y -ad unrar_test_23.rar
-./unrar x -y -ad unrar_test_24.rar
-./unrar x -y -ad -ola unrar_test_25.rar
-./unrar x -y -ad -ola unrar_test_26.rar
-./unrar x -y -ad unrar_test_27.rar
-./unrar x -y -ad unrar_test_28.rar
-./unrar x -y -ad unrar_test_29.rar
-./unrar x -y -ad unrar_test_30.rar
-./unrar x -y -ad unrar_test_31.rar
-./unrar x -y -ad -ola unrar_test_32.rar
-./unrar x -y -ad unrar_test_33.rar
-./unrar x -y -ad unrar_test_34.rar
-./unrar x -y -ad unrar_test_35.rar
-./unrar x -y -ad unrar_test_36.rar
-./unrar x -y -ad unrar_test_37.rar
-./unrar x -y -ad unrar_test_38.rar
-./unrar x -y -ad unrar_test_39.rar
-./unrar x -y -ad unrar_test_40.rar
-./unrar x -y -ad unrar_test_41.rar
-./unrar x -y -ad unrar_test_42.rar
-./unrar x -y -ad unrar_test_43.rar
-./unrar x -y -ad unrar_test_44.rar
-./unrar x -y -ad unrar_test_45.rar
-./unrar x -y -ad unrar_test_46.rar
+rm -rf expected
+mkdir expected
+./unrar x -y -ad unrar_test_01.rar expected
+./unrar x -y -ad unrar_test_02.rar expected
+./unrar x -y -ad unrar_test_03.rar expected
+./unrar x -y -ad -pqwerty unrar_test_04.rar expected
+./unrar x -y -ad -pqwerty unrar_test_05.rar expected
+./unrar x -y -ad unrar_test_06.rar expected
+./unrar x -y -ad unrar_test_07.rar expected
+./unrar x -y -ad unrar_test_08.rar expected
+./unrar x -y -ad unrar_test_09.rar expected
+./unrar x -y -ad unrar_test_10.rar expected
+./unrar x -y -ad unrar_test_11.rar expected
+./unrar x -y -ad unrar_test_12.rar expected
+./unrar x -y -ad unrar_test_13.rar expected
+./unrar x -y -ad unrar_test_14.rar expected
+./unrar x -y -ad unrar_test_15.rar expected
+./unrar x -y -ad unrar_test_16.rar expected
+./unrar x -y -ad unrar_test_17.part1.rar expected
+./unrar x -y -ad unrar_test_18.rar expected
+./unrar x -y -ad -pżółć unrar_test_19.rar expected
+./unrar x -y -ad unrar_test_20.rar expected
+./unrar x -y -ad unrar_test_21.rar expected
+./unrar x -y -ad -pżółć unrar_test_22.rar expected
+./unrar x -y -ad unrar_test_23.rar expected
+./unrar x -y -ad unrar_test_24.rar expected
+./unrar x -y -ad -ola unrar_test_25.rar expected
+./unrar x -y -ad -ola unrar_test_26.rar expected
+./unrar x -y -ad unrar_test_27.rar expected
+./unrar x -y -ad unrar_test_28.rar expected
+./unrar x -y -ad unrar_test_29.rar expected
+./unrar x -y -ad unrar_test_30.rar expected
+./unrar x -y -ad unrar_test_31.rar expected
+./unrar x -y -ad -ola unrar_test_32.rar expected
+./unrar x -y -ad unrar_test_33.rar expected
+./unrar x -y -ad unrar_test_34.rar expected
+./unrar x -y -ad unrar_test_35.rar expected
+./unrar x -y -ad unrar_test_36.rar expected
+./unrar x -y -ad unrar_test_37.rar expected
+./unrar x -y -ad unrar_test_38.rar expected
+./unrar x -y -ad unrar_test_39.rar expected
+./unrar x -y -ad unrar_test_40.rar expected
+./unrar x -y -ad unrar_test_41.rar expected
+./unrar x -y -ad unrar_test_42.rar expected
+./unrar x -y -ad unrar_test_43.rar expected
+./unrar x -y -ad unrar_test_44.rar expected
+./unrar x -y -ad unrar_test_45.rar expected
+./unrar x -y -ad unrar_test_46.rar expected
+mkdir expected/unrar_test_46
+./unrar x -y -ad unrar_test_47.rar expected
+
+rm expected.rar
+./rar a -ola -oh expected.rar expected
